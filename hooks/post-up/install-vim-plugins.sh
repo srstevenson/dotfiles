ff72955d653a18a2d02eb8ed8d22d5ae98d17347
Scott Stevenson <scott@stevenson.io>
2016-05-15 22:49:46 +0100

    hooks: Name post-up hook informatively

--- hooks/post-up/install-vim-plugins.sh
+++ hooks/post-up/install-vim-plugins.sh
@@ -7,10 +7,10 @@ plug_src=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p "$(dirname "$plug_dst")"
 
-if command -v curl > /dev/null; then
-    curl -sfLo "$plug_dst" $plug_src
+if command -v curl >/dev/null; then
+  curl -sfLo "$plug_dst" $plug_src
 else
-    wget -qO "$plug_dst" $plug_src
+  wget -qO "$plug_dst" $plug_src
 fi
 
 vim -c PlugClean! -c PlugUpdate -c quitall
