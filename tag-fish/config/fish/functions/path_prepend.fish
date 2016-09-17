143335c36a1cb0e7738686a40974ebceb9110188
Scott Stevenson <scott@stevenson.io>
2016-09-18 00:26:53 +0100

    fish: Ensure $PATH has global scope

--- tag-fish/config/fish/functions/path_prepend.fish
+++ tag-fish/config/fish/functions/path_prepend.fish
@@ -2,6 +2,6 @@
 
 function path_prepend -d 'If a directory exists prepend it to $PATH'
     if test -d $argv[1]
-        set -x PATH $argv[1] $PATH
+        set -gx PATH $argv[1] $PATH
     end
 end
