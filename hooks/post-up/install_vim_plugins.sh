037eac837122aeeeabc451f81092f434edfc2aab
Scott Stevenson <scott@stevenson.io>
2016-09-17 22:45:05 +0100

    hooks,git,scripts,vim,zsh: Four space indentation

--- hooks/post-up/install_vim_plugins.sh
+++ hooks/post-up/install_vim_plugins.sh
@@ -3,5 +3,5 @@
 set -eu
 
 curl --create-dirs -fLSso "$HOME"/.vim/autoload/plug.vim \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c PlugClean! -c PlugUpdate -c qa
