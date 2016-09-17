49b90ee2d75281061d8642231c319a8bf5ff0438
Scott Stevenson <scott@stevenson.io>
2016-09-17 22:40:54 +0100

    hooks: Show curl(1) errors but not progress bar

--- hooks/post-up/install_vim_plugins.sh
+++ hooks/post-up/install_vim_plugins.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-curl --create-dirs -fLo "$HOME"/.vim/autoload/plug.vim \
+curl --create-dirs -fLSso "$HOME"/.vim/autoload/plug.vim \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c PlugClean! -c PlugUpdate -c qa
