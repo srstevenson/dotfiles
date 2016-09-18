5eb98a4049343047f11ed493e8d6a17a274d9ceb
Scott Stevenson <scott@stevenson.io>
2016-09-18 09:18:13 +0100

    fish: Make function descriptions less verbose

--- tag-fish/config/fish/functions/try_source.fish
+++ tag-fish/config/fish/functions/try_source.fish
@@ -1,6 +1,6 @@
 # ~/.config/fish/functions/try_source.fish
 
-function try_source -d 'Source a file if it is readable'
+function try_source -d 'Source a file if readable'
     if test -r $argv[1]
         source $argv[1]
     end
