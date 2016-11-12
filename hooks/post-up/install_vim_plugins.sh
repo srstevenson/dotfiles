cd995f162ac1daf49c4ddb4846557c8a02ee1841
Scott Stevenson <scott@stevenson.io>
2016-11-12 00:37:22 +0000

    hooks,fish,nvim,rcm,vim: Migrate from Vim to Neovim

--- hooks/post-up/install_vim_plugins.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-curl --create-dirs -fLSso "$HOME"/.vim/autoload/plug.vim \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim -c PlugClean! -c PlugUpdate -c qa
