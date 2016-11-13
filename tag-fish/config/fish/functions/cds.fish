5d26d7cc7e2951e70b48f63992e210649be38f89
Scott Stevenson <scott@stevenson.io>
2016-11-13 18:18:30 +0000

    fish: Refactor cds to not use temporary file

--- tag-fish/config/fish/functions/cds.fish
+++ tag-fish/config/fish/functions/cds.fish
@@ -1,8 +1,6 @@
 # ~/.config/fish/functions/cds.fish
 
 function cds -d 'Change directory to a source code repository'
-    set -l file (mktemp)
-    git ls-repos | fzy > $file
-    cd ~/src/(cat $file)
-    rm $file
+    git ls-repos | fzy | read -l result
+    and cd ~/src/$result
 end
