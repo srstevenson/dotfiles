9617f850e3a1ce2b62f28483732c8f38d421c2b8
Scott Stevenson <scott@stevenson.io>
2016-12-08 09:43:24 +0000

    nvim: Move plug.vim to $XDG_DATA_HOME

--- hooks/post-up/install_neovim_plugins.sh
+++ hooks/post-up/install_neovim_plugins.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-curl --create-dirs -fLSso "$HOME"/.config/nvim/autoload/plug.vim \
+curl --create-dirs -fLSso "$HOME"/.local/share/nvim/site/autoload/plug.vim \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim -c PlugClean! -c PlugUpdate -c qall
