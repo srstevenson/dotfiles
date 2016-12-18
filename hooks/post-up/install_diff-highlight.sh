60cca7f18d84f9343db10ec91e44be6a670c3f0e
Scott Stevenson <scott@stevenson.io>
2016-12-18 12:22:41 +0000

    hooks: Use symbolic notation to set execute bit

--- hooks/post-up/install_diff-highlight.sh
+++ hooks/post-up/install_diff-highlight.sh
@@ -4,4 +4,4 @@ set -eu
 
 curl --create-dirs -fLSso "$HOME"/.local/bin/diff-highlight \
   https://git.kernel.org/cgit/git/git.git/plain/contrib/diff-highlight/diff-highlight
-chmod 0700 "$HOME"/.local/bin/diff-highlight
+chmod +x "$HOME"/.local/bin/diff-highlight
