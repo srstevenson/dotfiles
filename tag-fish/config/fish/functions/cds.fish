e01171dc2e90eb7ead244a83f063e61061516ee2
Scott Stevenson <scott@stevenson.io>
2017-01-06 22:06:30 +0000

    fish,scripts: Inline git-ls-repos in cds
    git-ls-repos is only used by cds, therefore it's unnecessary to have its
    functionality in a standalone script.

--- tag-fish/config/fish/functions/cds.fish
+++ tag-fish/config/fish/functions/cds.fish
@@ -1,6 +1,7 @@
 # ~/.config/fish/functions/cds.fish
 
 function cds -d 'Change directory to a source code repository'
-    git ls-repos | fzy | read -l result
+    find ~/src -type d -name .git -execdir pwd \; | sed "s,$HOME/src/,," | \
+    fzy | read -l result
     and cd ~/src/$result
 end
