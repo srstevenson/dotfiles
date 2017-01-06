0362a6afe7535376f54c744104d130953e8e1b1d
Scott Stevenson <scott@stevenson.io>
2017-01-06 22:10:42 +0000

    fish: Use string builtin instead of sed(1)

--- tag-fish/config/fish/functions/cds.fish
+++ tag-fish/config/fish/functions/cds.fish
@@ -1,7 +1,7 @@
 # ~/.config/fish/functions/cds.fish
 
 function cds -d 'Change directory to a source code repository'
-    find ~/src -type d -name .git -execdir pwd \; | sed "s,$HOME/src/,," | \
-    fzy | read -l result
+    find ~/src -type d -name .git -execdir pwd \; | \
+    string replace "$HOME/src/" "" | fzy | read -l result
     and cd ~/src/$result
 end
