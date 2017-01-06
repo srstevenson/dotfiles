620fd9ffcaffefb542357c96dd57eece5d40e218
Scott Stevenson <scott@stevenson.io>
2017-01-06 22:12:19 +0000

    fish: Allow an initial query to be passed to fzy(1)

--- tag-fish/config/fish/functions/cds.fish
+++ tag-fish/config/fish/functions/cds.fish
@@ -1,7 +1,7 @@
 # ~/.config/fish/functions/cds.fish
 
-function cds -d 'Change directory to a source code repository'
+function cds -a query -d 'Change directory to a source code repository'
     find ~/src -type d -name .git -execdir pwd \; | \
-    string replace "$HOME/src/" "" | fzy | read -l result
+    string replace "$HOME/src/" "" | fzy -q "$query" | read -l result
     and cd ~/src/$result
 end
