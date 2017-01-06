cfdbc2aab152809068adf99a261751e238f058b0
Scott Stevenson <scott@stevenson.io>
2017-01-06 22:21:47 +0000

    fish: Cache system hostname in fish_title

--- tag-fish/config/fish/functions/fish_title.fish
+++ tag-fish/config/fish/functions/fish_title.fish
@@ -1,7 +1,11 @@
 # ~/.config/fish/functions/fish_title.fish
 
 function fish_title -a command -d 'Set the terminal title'
-    echo -ns (hostname -s) ': '
+    if not set -q __fish_title_hostname
+        set -g __fish_title_hostname (hostname -s)
+    end
+
+    echo -ns $__fish_title_hostname ': '
     if test $_ = fish
         prompt_pwd
     else
