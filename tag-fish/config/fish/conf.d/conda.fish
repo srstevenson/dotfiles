ba1d248ba3db08ff70d6112600b364b81f43254f
Scott Stevenson <scott@stevenson.io>
2017-01-07 22:33:08 +0000

    fish: Don't activate already active environment

--- tag-fish/config/fish/conf.d/conda.fish
+++ tag-fish/config/fish/conf.d/conda.fish
@@ -7,7 +7,11 @@ function __activate_conda_env -v PWD
     while test $__dir != $HOME; and test $__dir != /
         if test -f $__dir/.conda-env
             set __env (cat $__dir/.conda-env)
-            activate $__env
+
+            set -q $CONDA_DEFAULT_ENV
+            and test "$CONDA_DEFAULT_ENV" != $__env
+            and activate $__env
+
             break
         end
 
