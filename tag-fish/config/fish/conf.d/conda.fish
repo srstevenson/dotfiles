be76a8b3110bf0dad63b9a95d5f9e0f9101da774
Scott Stevenson <scott@stevenson.io>
2017-01-07 22:06:03 +0000

    fish: Automatically {,de}activate conda environments
    Each time the working directory changes in fish, the new working
    directory and all its parents up to $HOME are checked for the presence
    of a file named `.conda-env`. This is expected to be a text file
    containing the name of an existing conda environment followed by a
    single newline character.
    
    If a `.conda-env` file is found, the conda environment named within it
    is activated. Upon moving to another directory without a `.conda-env`
    file in it or any of its parents up to $HOME, any active conda
    environment is deactivated.

---
+++ tag-fish/config/fish/conf.d/conda.fish
@@ -0,0 +1,20 @@
+# ~/.config/fish/conf.d/conda.fish
+
+function __activate_conda_env -v PWD
+    set -l __dir $PWD
+    set -l __env ""
+
+    while test $__dir != $HOME; and test $__dir != /
+        if test -f $__dir/.conda-env
+            set __env (cat $__dir/.conda-env)
+            activate $__env
+            break
+        end
+
+        set __dir (dirname $__dir)
+    end
+
+    set -q CONDA_DEFAULT_ENV
+    and test -z $__env
+    and deactivate
+end
