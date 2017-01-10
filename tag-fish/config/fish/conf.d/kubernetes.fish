41bf07851aea03fdaf89165fe1b3fe2b3a778968
Scott Stevenson <scott@stevenson.io>
2017-01-10 07:44:40 +0000

    fish: Move configuration into per-tool files

---
+++ tag-fish/config/fish/conf.d/kubernetes.fish
@@ -0,0 +1,4 @@
+# ~/.config/fish/conf.d/kubernetes.fish
+
+abbr -a kgc kubectl config current-context
+abbr -a ksc kubectl config set current-context
--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -17,41 +17,7 @@ abbr -a la ls -Ahl
 abbr -a ll ls -hl
 abbr -a ls ls -h
 
-abbr -a dim docker images
-abbr -a dpa docker ps -a
-abbr -a dps docker ps
-abbr -a dri docker rmi
-abbr -a drm docker rm
-abbr -a dst docker stop
-
-abbr -a gad git add
-abbr -a gap git add -p
-abbr -a gbd git branch -d
-abbr -a gbr git branch
-abbr -a gca git commit --amend -v
-abbr -a gcb git checkout -b
-abbr -a gcf git clean -dfx
-abbr -a gci git commit -v
-abbr -a gcn git clean -dnx
-abbr -a gco git checkout
-abbr -a gdc git diff --cached
-abbr -a gdi git diff
-abbr -a gfp git fetch
-abbr -a glg git log --graph
-abbr -a glo git log --format=glo --graph
-abbr -a gpo git push origin
-abbr -a gra git rebase --abort
-abbr -a grc git rebase --continue
-abbr -a gri git rebase -i
-abbr -a grs git rebase --skip
-abbr -a gst git status -bs
-
-abbr -a kcf kubectl create -f
-abbr -a kgc kubectl config current-context
-abbr -a ksc kubectl config set current-context
-
 abbr -a cdd cd ~/.dotfiles
-abbr -a scala amm
 
 source_if_readable ~/.local/miniconda/etc/fish/conf.d/conda.fish
 
