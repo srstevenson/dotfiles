b7438886201086d2204f52b99dfb12b53b95d0db
Scott Stevenson <scott@stevenson.io>
2017-01-10 07:44:40 +0000

    fish: Move configuration into per-tool files

---
+++ tag-fish/config/fish/conf.d/docker.fish
@@ -0,0 +1,8 @@
+# ~/.config/fish/conf.d/docker.fish
+
+abbr -a dim docker images
+abbr -a dpa docker ps -a
+abbr -a dps docker ps
+abbr -a dri docker rmi
+abbr -a drm docker rm
+abbr -a dst docker stop
