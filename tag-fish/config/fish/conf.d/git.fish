30c219280c470c7773d437ecb0966023275987bd
Scott Stevenson <scott@stevenson.io>
2017-01-10 07:44:40 +0000

    fish: Move configuration into per-tool files

---
+++ tag-fish/config/fish/conf.d/git.fish
@@ -0,0 +1,16 @@
+# ~/.config/fish/conf.d/git.fish
+
+abbr -a gad git add
+abbr -a gbr git branch
+abbr -a gca git commit --amend
+abbr -a gci git commit
+abbr -a gco git checkout
+abbr -a gdc git diff --cached
+abbr -a gdi git diff
+abbr -a glg git log --graph --stat
+abbr -a glo git log --format=glo --graph
+abbr -a gra git rebase --abort
+abbr -a grc git rebase --continue
+abbr -a gri git rebase -i
+abbr -a grs git rebase --skip
+abbr -a gst git status -bs
