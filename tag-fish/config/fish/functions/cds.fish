0b596795791621d380ae62f72ae3d614d4fd89c1
Scott Stevenson <scott@stevenson.io>
2017-01-10 08:44:02 +0000

    fish: Don't execute pwd in each directory in cds

--- tag-fish/config/fish/functions/cds.fish
+++ tag-fish/config/fish/functions/cds.fish
@@ -1,7 +1,8 @@
 # ~/.config/fish/functions/cds.fish
 
 function cds -a query -d 'Change directory to a source code repository'
-    find ~/src -type d -name .git -execdir pwd \; | \
-    string replace "$HOME/src/" "" | fzy -q "$query" | read -l result
+    find ~/src -type d -name .git | \
+    string replace -r "^$HOME/src/(.*)/\.git\$" '$1' | fzy -q "$query" | \
+    read -l result
     and cd ~/src/$result
 end
