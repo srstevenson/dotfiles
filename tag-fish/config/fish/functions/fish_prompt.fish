3fed84116f8495bb8e74d418f8b86c5bec98532e
Scott Stevenson <scott@stevenson.io>
2017-01-29 21:33:21 +0000

    fish: Show hostname in prompt

--- tag-fish/config/fish/functions/fish_prompt.fish
+++ tag-fish/config/fish/functions/fish_prompt.fish
@@ -4,9 +4,11 @@ function fish_prompt -d 'Print the command prompt'
     set -g __fish_git_prompt_show_informative_status 1
     set -g __fish_git_prompt_showcolorhints 1
 
-    set_color blue
-    echo -n (prompt_pwd)
-    set_color normal
+    if not set -q __fish_prompt_hostname
+        set -g __fish_prompt_hostname (hostname -s)
+    end
+
+    echo -ns (set_color cyan) $__fish_prompt_hostname (set_color normal) ':' (prompt_pwd)
     __fish_git_prompt '(%s)'
     echo -n '❯ '
 end
