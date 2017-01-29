f6e3844f32531743adb279714002fb6701671b9c
Scott Stevenson <scott@stevenson.io>
2017-01-29 21:21:05 +0000

    hooks: Use tilde expansion for home directory

--- hooks/post-up/install_diff-highlight.sh
+++ hooks/post-up/install_diff-highlight.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-curl --create-dirs -fLSso "$HOME"/.local/bin/diff-highlight \
+curl --create-dirs -fLSso ~/.local/bin/diff-highlight \
   https://git.kernel.org/cgit/git/git.git/plain/contrib/diff-highlight/diff-highlight
-chmod +x "$HOME"/.local/bin/diff-highlight
+chmod +x ~/.local/bin/diff-highlight
