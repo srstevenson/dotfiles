40a97e5d3a85cde04661f574e7d31e6d9912b33c
Scott Stevenson <scott@stevenson.io>
2017-01-29 21:32:27 +0000

    fish: Change function description

--- tag-fish/config/fish/functions/fish_prompt.fish
+++ tag-fish/config/fish/functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 # ~/.config/fish/functions/fish_prompt.fish
 
-function fish_prompt -d 'Write out the prompt'
+function fish_prompt -d 'Print the command prompt'
     set -g __fish_git_prompt_show_informative_status 1
     set -g __fish_git_prompt_showcolorhints 1
 
