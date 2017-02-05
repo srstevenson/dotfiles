751816bcc6c175da62afa5cb46cc7e46fb11c9ce
Scott Stevenson <scott@stevenson.io>
2017-02-05 12:14:32 +0000

    fish: Only cache hostname once

--- tag-fish/config/fish/functions/fish_title.fish
+++ tag-fish/config/fish/functions/fish_title.fish
@@ -1,11 +1,11 @@
 # ~/.config/fish/functions/fish_title.fish
 
 function fish_title -a command -d 'Set the terminal title'
-    if not set -q __fish_title_hostname
-        set -g __fish_title_hostname (hostname -s)
+    if not set -q __fish_prompt_hostname
+        set -g __fish_prompt_hostname (hostname -s)
     end
 
-    echo -ns $__fish_title_hostname ': '
+    echo -ns $__fish_prompt_hostname ': '
     if test $_ = fish
         prompt_pwd
     else
