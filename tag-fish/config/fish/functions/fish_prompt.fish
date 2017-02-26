89abcd04adca8294ab3dc44f26afd405053d5d3c
Scott Stevenson <scott@stevenson.io>
2017-02-26 19:16:25 +0000

    fish: Refactor setting prompt character

--- tag-fish/config/fish/functions/fish_prompt.fish
+++ tag-fish/config/fish/functions/fish_prompt.fish
@@ -1,11 +1,11 @@
 # ~/.config/fish/functions/fish_prompt.fish
 
 function fish_prompt -d 'Print the command prompt'
-    set -l __fish_prompt_character
+    set -l __fish_prompt_char_colour
     if test $status -ne 0
-        set __fish_prompt_character (set_color red) '❯ ' (set_color normal)
+        set __fish_prompt_char_colour red
     else
-        set __fish_prompt_character '❯ '
+        set __fish_prompt_char_colour normal
     end
 
     set -g __fish_git_prompt_show_informative_status 1
@@ -15,8 +15,8 @@ function fish_prompt -d 'Print the command prompt'
         set -g __fish_prompt_hostname (hostname -s)
     end
 
-    echo -ns (set_color purple) $__fish_prompt_hostname (set_color normal) \
-        ':' (set_color cyan) (prompt_pwd) (set_color normal)
+    echo -ns (set_color green) $__fish_prompt_hostname (set_color normal) ':' \
+        (set_color blue) (prompt_pwd) (set_color normal)
     __fish_git_prompt '(%s)'
-    echo -ns $__fish_prompt_character
+    echo -ns (set_color $__fish_prompt_char_colour) '$ ' (set_color normal)
 end
