b47eae122381f06f6ff0f2bef39911ce65ebdfb4
Scott Stevenson <scott@stevenson.io>
2017-04-25 08:08:35 +0100

    fish: Show entity in fzy prompts

--- tag-fish/config/fish/functions/k8scontext.fish
+++ tag-fish/config/fish/functions/k8scontext.fish
@@ -1,5 +1,6 @@
 # ~/.config/fish/functions/k8scontext.fish
 
 function k8scontext -d 'Set kubectl context'
-    kubectl config get-contexts -o name | fzy | xargs kubectl config use-context
+    kubectl config get-contexts -o name | fzy -p 'context> ' | \
+        xargs kubectl config use-context
 end
--- tag-fish/config/fish/functions/k8slogs.fish
+++ tag-fish/config/fish/functions/k8slogs.fish
@@ -1,7 +1,9 @@
 # ~/.config/fish/functions/k8slogs.fish
 
 function k8slogs -d 'Follow logs of a Kubernetes pod'
-    kubectl get namespaces --no-headers | fzy | awk '{ print $1 }' | read -l namespace
-    kubectl get pods --no-headers --namespace $namespace | fzy | awk '{ print $1 }' | read -l pod
+    kubectl get namespaces --no-headers | fzy -p 'namespace> ' | \
+        awk '{ print $1 }' | read -l namespace
+    kubectl get pods --no-headers --namespace $namespace | fzy -p 'pod> ' | \
+        awk '{ print $1 }' | read -l pod
     kubectl logs -f --since 24h --namespace $namespace $pod
 end
