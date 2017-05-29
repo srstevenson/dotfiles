c15b1a38cee5a08e2b835caaf536280b3bb527d6
Scott Stevenson <scott@stevenson.io>
2017-05-29 08:24:16 +0100

    fish: Indent exclusively with spaces

--- tag-fish/config/fish/functions/cds.fish
+++ tag-fish/config/fish/functions/cds.fish
@@ -2,6 +2,6 @@
 
 function cds -d 'Change directory to a source code repository'
     find ~/src -type d -name .git | \
-	string replace -r "^$HOME/src/(.*)/\.git\$" '$1' | fzy | read -l result
+        string replace -r "^$HOME/src/(.*)/\.git\$" '$1' | fzy | read -l result
     and cd ~/src/$result
 end
