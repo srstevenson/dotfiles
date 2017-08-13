7621d040e143190d8437e01326e37c7f38d46b8c
Scott Stevenson <scott@stevenson.io>
2017-08-13 16:44:56 +0100

    fish: Remove unused Git abbreviations

--- tag-fish/config/fish/conf.d/git.fish
+++ tag-fish/config/fish/conf.d/git.fish
@@ -1,20 +1,16 @@
 # ~/.config/fish/conf.d/git.fish
 
 abbr -a gad git add
-abbr -a gap git add -p
-abbr -a gbm git branch -m
 abbr -a gbr git branch
 abbr -a gca git commit --amend -v
-abbr -a gcb git checkout -b
 abbr -a gci git commit -v
 abbr -a gco git checkout
+abbr -a gdc git diff --cached
 abbr -a gdi git diff
-abbr -a gds git diff --staged
-abbr -a gfa git fetch --all
-abbr -a glm git log --graph
+abbr -a glg git log --graph
 abbr -a glo git log --format=glo --graph
 abbr -a gra git rebase --abort
 abbr -a grc git rebase --continue
 abbr -a gri git rebase -i
 abbr -a grs git rebase --skip
-abbr -a gst git status -bs
+abbr -a gst git status
