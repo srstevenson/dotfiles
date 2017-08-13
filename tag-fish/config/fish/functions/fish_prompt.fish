f3bf4e8254fae0e1130bfd53888e885690ec360a
Scott Stevenson <scott@stevenson.io>
2017-08-13 16:37:50 +0100

    fish: Simplify left prompt

--- tag-fish/config/fish/functions/fish_prompt.fish
+++ tag-fish/config/fish/functions/fish_prompt.fish
@@ -1,22 +1,18 @@
 # ~/.config/fish/functions/fish_prompt.fish
 
-function fish_prompt -d 'Print the command prompt'
-    set -l __fish_prompt_char_colour
-    if test $status -ne 0
-        set __fish_prompt_char_colour red
-    else
-        set __fish_prompt_char_colour normal
+function fish_prompt -d 'Print the left prompt'
+    if set -q SSH_TTY; and set -q TMUX
+        set -gx SSH_AUTH_SOCK (tmux showenv SSH_AUTH_SOCK | cut -d = -f 2)
     end
-
-    set -g __fish_git_prompt_show_informative_status 1
-    set -g __fish_git_prompt_showcolorhints 1
 
     if not set -q __fish_prompt_hostname
         set -g __fish_prompt_hostname (hostname -s)
     end
 
-    echo -ns (set_color green) $__fish_prompt_hostname (set_color normal) ':' \
-        (set_color blue) (prompt_pwd) (set_color normal)
-    __fish_git_prompt '(%s)'
-    echo -ns (set_color $__fish_prompt_char_colour) '$ ' (set_color normal)
+    set -g __fish_git_prompt_show_informative_status 1
+    set -g __fish_git_prompt_showcolorhints 1
+
+    echo -ns (set_color green) $__fish_prompt_hostname \
+        (set_color normal) ':' (set_color blue) (prompt_pwd) \
+        (set_color normal) (__fish_git_prompt '(%s)') '$ '
 end
