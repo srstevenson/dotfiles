d8f8b0bfcf632a84f97a7287b0341c5fa335b65b
Scott Stevenson <scott@stevenson.io>
2017-08-13 16:31:53 +0100

    hooks: Migrate to single pre-up hook

--- hooks/post-up/1-install_neovim_plugins.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-curl --create-dirs -fLSso ~/.local/share/nvim/site/autoload/plug.vim \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim -c PlugClean! -c PlugUpdate -c qall
