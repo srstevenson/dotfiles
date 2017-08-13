d3533ec041d6d2ee01851c87b861485395f6a49e
Scott Stevenson <scott@stevenson.io>
2017-08-13 16:34:15 +0100

    fish: Remove k8slogs for Python implementation

--- tag-fish/config/fish/functions/k8slogs.fish
+++
@@ -1,9 +0,0 @@
-# ~/.config/fish/functions/k8slogs.fish
-
-function k8slogs -d 'Follow logs of a Kubernetes pod'
-    kubectl get namespaces --no-headers | fzy -p 'namespace> ' | \
-        awk '{ print $1 }' | read -l namespace
-    kubectl get pods --no-headers --namespace $namespace | fzy -p 'pod> ' | \
-        awk '{ print $1 }' | read -l pod
-    kubectl logs -f --since 24h --namespace $namespace $pod
-end
