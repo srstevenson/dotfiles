db0c6799458e9b60df895484cebe90411abe8543
Scott Stevenson <scott@stevenson.io>
2017-12-18 23:14:03 +0000

    fish: Remove k8scontext function

--- tag-fish/config/fish/functions/k8scontext.fish
+++
@@ -1,6 +0,0 @@
-# ~/.config/fish/functions/k8scontext.fish
-
-function k8scontext -d 'Set kubectl context'
-    kubectl config get-contexts -o name | fzy -p 'context> ' | \
-        xargs kubectl config use-context
-end
