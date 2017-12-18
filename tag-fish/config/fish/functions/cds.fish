4a75cdf2f0b6e654a4e0d8d566af0cd05241cc1f
Scott Stevenson <scott@stevenson.io>
2017-12-18 23:08:18 +0000

    fish: Remove cds function

--- tag-fish/config/fish/functions/cds.fish
+++
@@ -1,7 +0,0 @@
-# ~/.config/fish/functions/cds.fish
-
-function cds -d 'Change directory to a source code repository'
-    find ~/src -type d -name .git | \
-        string replace -r "^$HOME/src/(.*)/\.git\$" '$1' | fzy | read -l result
-    and cd ~/src/$result
-end
