846747a29dbdcea37229d9b91d977ba5207f9b4b
Scott Stevenson <scott@stevenson.io>
2018-01-12 13:45:50 +0000

    fish: Add parentheses around Kubernetes context

--- tag-fish/config/fish/functions/fish_right_prompt.fish
+++ tag-fish/config/fish/functions/fish_right_prompt.fish
@@ -3,7 +3,7 @@
 function fish_right_prompt -d 'Write out the right prompt'
     if test -e ~/.kube/config
         set_color brblack
-        echo -ns " " (kubectl config current-context | cut -d . -f 1)
+        echo -ns "(" (kubectl config current-context | cut -d . -f 1) ")"
         set_color normal
     end
 end
