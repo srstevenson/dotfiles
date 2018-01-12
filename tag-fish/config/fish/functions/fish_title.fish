dd64f89344e89c48e4fc031d1890551cf3a66e8d
Scott Stevenson <scott@stevenson.io>
2018-01-12 13:46:40 +0000

    fish: Use predefined prompt_hostname function

--- tag-fish/config/fish/functions/fish_title.fish
+++ tag-fish/config/fish/functions/fish_title.fish
@@ -1,11 +1,7 @@
 # ~/.config/fish/functions/fish_title.fish
 
-function fish_title -a command -d 'Set the terminal title'
-    if not set -q __fish_title_hostname
-        set -g __fish_title_hostname (hostname -s)
-    end
-
-    echo -ns $__fish_title_hostname ': '
+function fish_title -a command -d 'Write out the terminal title'
+    echo -ns (prompt_hostname) ': '
 
     if test $_ = fish
         prompt_pwd
