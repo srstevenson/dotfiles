a28fb4501aa2548a6d1afa09ba4fbc7dc9e7c64b
Scott Stevenson <scott@stevenson.io>
2018-01-13 17:31:35 +0000

    fish,git,nvim: Change colour scheme

--- tag-fish/config/fish/functions/fish_prompt.fish
+++ tag-fish/config/fish/functions/fish_prompt.fish
@@ -4,7 +4,7 @@ function fish_prompt -d 'Write out the prompt'
     set -g __fish_git_prompt_show_informative_status 1
     set -g __fish_git_prompt_showcolorhints 1
 
-    set_color blue
+    set_color cyan
     echo -n (prompt_pwd)
     set_color normal
     __fish_git_prompt '(%s)'
