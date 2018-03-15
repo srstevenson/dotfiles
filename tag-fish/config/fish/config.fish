cccd6b4a976363a811abe1d3144ac0dca8359beb
Scott Stevenson <scott@stevenson.io>
2018-03-15 14:59:26 +0000

    fish: Set and export PIPSI_HOME

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -21,6 +21,7 @@ set -gx EDITOR nvim
 set -gx LESS FimRX
 set -gx GPG_TTY (tty)
 set -gx HOMEBREW_NO_ANALYTICS 1
+set -gx PIPSI_HOME ~/.local/share/virtualenvs
 
 abbr -a la ls -Ahl
 abbr -a ll ls -hl
