52eac458076db26aea11c65379de37e8874a5094
Scott Stevenson <scott@stevenson.io>
2018-03-15 15:50:22 +0000

    fish: Add TeX Live installation to path

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -11,6 +11,7 @@ end
 
 set fish_greeting
 
+prepend_to_path ~/opt/texlive/2017/bin/x86_64-(uname -s | tr 'A-Z' 'a-z')
 prepend_to_path ~/.cargo/bin
 prepend_to_path ~/Dropbox/bin
 prepend_to_path ~/.local/bin
