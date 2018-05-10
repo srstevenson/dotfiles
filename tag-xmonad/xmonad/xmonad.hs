1ea37e7f09456cc40725a9b94db85b1d42967eca
Scott Stevenson <scott@stevenson.io>
2018-05-10 07:19:44 +0100

    xmonad: Send {Firefox,Enpass} to workspace {2,9}

--- tag-xmonad/xmonad/xmonad.hs
+++ tag-xmonad/xmonad/xmonad.hs
@@ -6,7 +6,14 @@ import XMonad.Util.EZConfig
 import XMonad.Util.Run
 
 main = xmonad $ mateConfig
-    { modMask = mod4Mask }
+    { modMask = mod4Mask
+    , manageHook = srsManageHook <+> manageHook mateConfig
+    }
     `additionalKeysP`
     [ ("M-S-l", safeSpawn "mate-screensaver-command" [ "--lock" ] )
     , ("<Print>", safeSpawnProg "mate-screenshot" ) ]
+
+srsManageHook = composeAll
+    [ className =? "Enpass-Desktop" --> doShift "9"
+    , className =? "Firefox"        --> doShift "2"
+    ]
