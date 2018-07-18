32cfe906a2e5b47fde6df581110ad3126fd67645
Scott Stevenson <scott@stevenson.io>
2018-07-18 17:57:52 +0100

    fish: Move Git abbreviations to config.fish

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -31,6 +31,24 @@ abbr -a ls ls -h
 
 abbr -a cdd cd ~/dotfiles
 
+abbr -a gad git add
+abbr -a gap git add -p
+abbr -a gbr git branch
+abbr -a gca git commit --amend
+abbr -a gci git commit
+abbr -a gco git checkout
+abbr -a gdc git diff --cached
+abbr -a gdi git diff
+abbr -a glg git log --graph
+abbr -a glo git log --format=glo --graph
+abbr -a gra git rebase --abort
+abbr -a grc git rebase --continue
+abbr -a gri git rebase -i
+abbr -a grs git rebase --skip
+abbr -a gsp git stash pop
+abbr -a gss git stash save
+abbr -a gst git status
+
 if type -q jump
     source (jump shell fish | psub)
 end
