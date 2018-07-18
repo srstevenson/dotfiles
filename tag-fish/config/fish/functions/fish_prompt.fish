ad29c0438785c094b50fe93046e9401d06bf1724
Scott Stevenson <scott@stevenson.io>
2018-07-18 17:22:35 +0100

    fish: Simplify left prompt

--- tag-fish/config/fish/functions/fish_prompt.fish
+++ tag-fish/config/fish/functions/fish_prompt.fish
@@ -4,16 +4,8 @@ function fish_prompt -d "Print the left prompt"
     set -g __fish_git_prompt_show_informative_status 1
     set -g __fish_git_prompt_showcolorhints 1
 
-    set_color cyan
     echo -n (prompt_pwd)
-    set_color normal
-    __fish_git_prompt '(%s)'
+    __fish_git_prompt "(%s)"
 
-    if jobs >/dev/null
-        set_color purple
-        echo -n '!'
-        set_color normal
-    end
-
-    echo -n '$ '
+    echo -n "> "
 end
