a2e56338b0b24a13e5dfdf86387c48fec827f58e
Scott Stevenson <scott@stevenson.io>
2018-07-18 17:30:06 +0100

    fish: Remove right prompt

--- tag-fish/config/fish/functions/fish_right_prompt.fish
+++
@@ -1,9 +0,0 @@
-# ~/.config/fish/functions/fish_right_prompt.fish
-
-function fish_right_prompt -d 'Write out the right prompt'
-    if test -e ~/.kube/config
-        set_color brblack
-        echo -ns "⎈ " (kubectl config current-context | cut -d . -f 1)
-        set_color normal
-    end
-end
