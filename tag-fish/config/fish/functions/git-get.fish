f989f03cfdf0d212680c7a3ed8462d6aa75975df
Scott Stevenson <scott@stevenson.io>
2018-12-21 07:42:56 +0000

    fish: Add SourceHut as Git host and default to GitHub

--- tag-fish/config/fish/functions/git-get.fish
+++ tag-fish/config/fish/functions/git-get.fish
@@ -5,23 +5,31 @@ function git-get -d "Clone a Git repository to the canonical path"
         case -b
             set host bitbucket.org
             set repo $argv[2]
+            set repo_prefix ""
+            set repo_suffix .git
         case -l
             set host gitlab.com
             set repo $argv[2]
-        case "-h"
+            set repo_prefix ""
+            set repo_suffix .git
+        case -s
+            set host git.sr.ht
+            set repo $argv[2]
+            set repo_prefix "~"
+            set repo_suffix ""
+        case "*"
             set host github.com
             set repo $argv[2]
-        case "*"
-            set host git.stevenson.io
-            set repo $argv[1]
+            set repo_prefix ""
+            set repo_suffix .git
     end
 
-    if string match -q git.stevenson.io $host; or string match -eq / $repo
-        set src git@$host:$repo.git
-        set dest ~/src/$host/$repo
+    if string match -eq / $repo
+        set src git@$host:$repo_prefix$repo$repo_suffix
+        set dest ~/src/$host/$repo_prefix$repo
     else
-        set src git@$host:srstevenson/$repo.git
-        set dest ~/src/$host/srstevenson/$repo
+        set src git@$host:{$repo_prefix}srstevenson/$repo$repo_suffix
+        set dest ~/src/$host/{$repo_prefix}srstevenson/$repo
     end
 
     if test -d $dest
