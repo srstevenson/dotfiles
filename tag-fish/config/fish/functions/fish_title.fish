8696349070d48482005a0385da8d255aa95982b9
Scott Stevenson <scott@stevenson.io>
2018-12-30 22:31:17 +0000

    fish: Upgrade configuration for fish 3.0.0
    - Source `jump` configuration from stdin.
    - Use `status current-command` in command substitution as `$_` variable
      has been deprecated.
    - Remove check that directories exist before prepending to the path, as
      non-existent directories no longer lead to a warning being printed.
    - Use new `--prepend` option of `set` to prepend directories to path.
    
    See the release notes [0] for further details.
    
    [0]: https://github.com/fish-shell/fish-shell/releases/tag/3.0.0

--- tag-fish/config/fish/functions/fish_title.fish
+++ tag-fish/config/fish/functions/fish_title.fish
@@ -5,7 +5,7 @@ function fish_title -a command -d 'Write out the terminal title'
     prompt_hostname
     echo -n ": "
 
-    if test $_ = fish
+    if test (status current-command) = fish
         prompt_pwd
     else
         echo -n $command
--- tag-fish/config/fish/functions/prepend_to_path.fish
+++ tag-fish/config/fish/functions/prepend_to_path.fish
@@ -1,9 +1,7 @@
 # ~/.config/fish/functions/prepend_to_path.fish
 
-function prepend_to_path -a dir -d 'Prepend a directory to the path if it exists'
+function prepend_to_path -a dir -d 'Prepend a directory to the path'
     if not contains $dir $PATH
-        if test -d $dir
-            set -gx PATH $dir $PATH
-        end
+        set -gpx PATH $dir
     end
 end
