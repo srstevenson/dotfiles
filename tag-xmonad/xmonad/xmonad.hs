c9d9be803462c5cda58ec0a12f465df1c1e0903d
Scott Stevenson <scott@stevenson.io>
2019-02-05 00:00:36 +0000

    xmonad: Format xmonad.hs with brittany

--- tag-xmonad/xmonad/xmonad.hs
+++ tag-xmonad/xmonad/xmonad.hs
@@ -1,16 +1,16 @@
 -- ~/.xmonad/xmonad.hs
 
-import XMonad
-import XMonad.Config.Desktop
-import XMonad.Util.EZConfig
-import XMonad.Util.Run
+import           XMonad
+import           XMonad.Config.Desktop
+import           XMonad.Util.EZConfig
+import           XMonad.Util.Run
 
-main = xmonad $ desktopConfig
-    { modMask = mod4Mask
-    , terminal = "alacritty" }
+main =
+  xmonad
+    $ desktopConfig { modMask = mod4Mask, terminal = "alacritty" }
     `additionalKeysP` srsAdditionalKeys
 
 srsAdditionalKeys =
-    [ ("M-S-l", safeSpawn "mate-screensaver-command" [ "--lock" ] )
-    , ("<Print>", safeSpawnProg "mate-screenshot" )
-    ]
+  [ ("M-S-l"  , safeSpawn "mate-screensaver-command" ["--lock"])
+  , ("<Print>", safeSpawnProg "mate-screenshot")
+  ]
