4c0a26de97a14613791f5d201a1adfae2b61cb09
Scott Stevenson <scott@stevenson.io>
2019-04-04 23:58:07 +0100

    bash,fish,rcm: Migrate from fish to bash

--- tag-fish/config/fish/functions/fish_mode_prompt.fish
+++
@@ -1,4 +0,0 @@
-# ~/.config/fish/functions/fish_mode_prompt.fish
-
-function fish_mode_prompt -d "Don't display the current mode"
-end
--- tag-fish/config/fish/functions/prepend_to_path.fish
+++
@@ -1,7 +0,0 @@
-# ~/.config/fish/functions/prepend_to_path.fish
-
-function prepend_to_path -a dir -d 'Prepend a directory to the path'
-    if not contains $dir $PATH
-        set -gpx PATH $dir
-    end
-end
