048d97f2ae85d461a5ff9dcd7f5f8a606e7a93f1
Scott Stevenson <scott@stevenson.io>
2022-05-04 19:26:50 +0100

    nvim: Create autocmds and user commands with Lua API
    This requires Neovim 0.7 or later.

--- tag-nvim/config/nvim/init.lua
+++ tag-nvim/config/nvim/init.lua
@@ -31,40 +31,36 @@ end
 
 vim.cmd("colorscheme base16")
 
-local function define_autocmds(group, definitions)
-    vim.cmd("augroup " .. group)
-    vim.cmd("autocmd!")
-    for _, definition in ipairs(definitions) do
-        vim.cmd(table.concat(vim.tbl_flatten{ "autocmd", definition }, " "))
+vim.api.nvim_create_autocmd("TextYankPost", {
+    pattern = "*",
+    callback = function()
+        vim.highlight.on_yank()
     end
-    vim.cmd("augroup end")
-end
-
-define_autocmds("highlight_on_yank", {
-    { "TextYankPost", "*", "lua vim.highlight.on_yank()" },
 })
 
-define_autocmds("spellcheck", {
-    { "FileType", "markdown,tex", "setlocal spell" }
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "gitcommit,markdown,tex",
+    callback = function()
+        vim.opt_local.spell = true
+    end
 })
 
-function cleanup_whitespace()
-    local view = vim.fn.winsaveview()
-    vim.cmd [[ %s/\s\+$//e ]]
-    vim.cmd [[ %s/\%^\n\+//e ]]
-    vim.cmd [[ %s/\n\+\%$//e ]]
-    vim.fn.winrestview(view)
-end
-
-define_autocmds("whitespace", {
-    { "BufWritePre", "*", "lua cleanup_whitespace()" }
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function()
+        local view = vim.fn.winsaveview()
+        vim.cmd [[ %s/\s\+$//e ]]
+        vim.cmd [[ %s/\%^\n\+//e ]]
+        vim.cmd [[ %s/\n\+\%$//e ]]
+        vim.fn.winrestview(view)
+    end
 })
 
-function delete_current_file()
-    local filename = vim.fn.expand("%")
-    vim.fn.delete(filename)
-    vim.api.nvim_buf_delete(0, { force = true })
-    vim.api.nvim_echo({{ "Deleted " .. filename }}, false, {})
-end
-
-vim.cmd("command! Delete lua delete_current_file()")
+vim.api.nvim_create_user_command("Delete", function()
+        local filename = vim.fn.expand("%")
+        vim.fn.delete(filename)
+        vim.api.nvim_buf_delete(0, { force = true })
+        vim.api.nvim_echo({{ "Deleted " .. filename }}, false, {})
+    end,
+    {}
+)
