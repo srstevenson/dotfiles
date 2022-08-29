1c1ca4adddaed3e3c43456ab850e015e92c4f8de
Scott Stevenson <scott@stevenson.io>
2022-08-29 12:59:49 +0100

    nvim: Enable spell checking for prose formats

--- tag-nvim/config/nvim/init.lua
+++ tag-nvim/config/nvim/init.lua
@@ -4,6 +4,7 @@ vim.o.expandtab = true
 vim.o.list = true
 vim.o.shiftwidth = 4
 vim.o.softtabstop = -1
+vim.o.spelllang = "en_gb"
 
 vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = "*",
@@ -13,5 +14,12 @@ vim.api.nvim_create_autocmd("BufWritePre", {
         vim.cmd [[ %s/\%^\n\+//e ]]
         vim.cmd [[ %s/\n\+\%$//e ]]
         vim.fn.winrestview(view)
+    end
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "gitcommit,markdown,tex,text",
+    callback = function()
+        vim.opt_local.spell = true
     end
 })
