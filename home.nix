cde8092678bf328016d978c84c208afddd171490
Scott Stevenson <scott@stevenson.io>
2023-06-16 21:49:22 +0100

    home-manager: Add support for Linux hosts

--- home.nix
+++ home.nix
@@ -4,7 +4,8 @@
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "scott";
-  home.homeDirectory = "/Users/scott";
+  home.homeDirectory = with pkgs.stdenv;
+    if isDarwin then "/Users/scott" else "/home/scott";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
