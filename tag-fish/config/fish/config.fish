3b0fe8f7217beea61636c5fa8cd1723de442fcb3
Scott Stevenson <scott@stevenson.io>
2024-03-21 22:08:24 +0000

    fish: Enable transient prompt

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -7,6 +7,7 @@ fish_add_path -P ~/bin ~/.local/bin ~/.cargo/bin
 set -gx VISUAL hx
 
 starship init fish | source
+enable_transience
 
 if test -n "$SSH_CONNECTION" -a -z "$TMUX"
     tmux attach; or tmux new-session
