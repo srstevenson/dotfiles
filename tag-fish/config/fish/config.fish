5d2fa2518270d0c5cd01f70e78784d63440b92fd
Scott Stevenson <scott@stevenson.io>
2024-03-26 19:51:56 +0000

    fish: Change multiplexer from tmux to Zellij

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -12,6 +12,10 @@ set -gx DFT_BACKGROUND light
 starship init fish | source
 enable_transience
 
-if test -n "$SSH_CONNECTION" -a -z "$TMUX" -a -z "$ZELLIJ"
-    tmux attach; or tmux new-session
+# if test -n "$SSH_CONNECTION" -a -z "$TMUX" -a -z "$ZELLIJ"
+#     tmux attach; or tmux new-session
+# end
+
+if test -n "$SSH_CONNECTION" -a -z "$ZELLIJ" -a -z "$TMUX"
+    zellij attach -c
 end
