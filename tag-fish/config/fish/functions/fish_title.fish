e15d232b15bfa1f4c3b34e7cfcf2148c5677ca31
Scott Stevenson <scott@stevenson.io>
2024-03-29 10:42:19 +0000

    fish: Use tab title as Zellij tab name without truncation

--- tag-fish/config/fish/functions/fish_title.fish
+++ tag-fish/config/fish/functions/fish_title.fish
@@ -1,15 +1,5 @@
 function fish_title
-    if set -q argv[1]
-        echo $argv[1]
-    else
-        prompt_pwd
-    end
-
-    if set -q ZELLIJ
-        if set -q argv[1]
-            zellij action rename-tab (string split " " $argv[1])[1]
-        else
-            zellij action rename-tab fish
-        end
-    end
+    set -l title (set -q argv[1]; and echo $argv[1]; or prompt_pwd)
+    set -q ZELLIJ; and zellij action rename-tab "$title"
+    echo "$title"
 end
