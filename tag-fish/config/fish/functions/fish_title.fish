d9787511278ec0680fe2debd5e883b07fe03ebed
Scott Stevenson <scott@stevenson.io>
2024-04-17 21:05:13 +0100

    fish: Disable setting Zellij terminal title
    The `action rename-tab` command is broken in Zellij v0.40.0:
    https://github.com/zellij-org/zellij/issues/3269

--- tag-fish/config/fish/functions/fish_title.fish
+++ tag-fish/config/fish/functions/fish_title.fish
@@ -1,5 +1,6 @@
 function fish_title
     set -l title (set -q argv[1]; and echo $argv[1]; or prompt_pwd)
-    set -q ZELLIJ; and zellij action rename-tab "$title"
+    # Broken in Zellij v0.40.0: https://github.com/zellij-org/zellij/issues/3269
+    # set -q ZELLIJ; and zellij action rename-tab "$title"
     echo "$title"
 end
