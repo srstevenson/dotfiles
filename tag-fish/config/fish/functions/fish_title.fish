1f26d75eab0299325e78065f71af134b0c229e85
Scott Stevenson <scott@stevenson.io>
2024-05-02 20:29:28 +0100

    fish: Set terminal title in Zellij
    The `zellij action rename-tab` command was broken in v0.40.0, but
    was fixed in v0.40.1 and so this can be re-enabled.

--- tag-fish/config/fish/functions/fish_title.fish
+++ tag-fish/config/fish/functions/fish_title.fish
@@ -1,6 +1,6 @@
 function fish_title
-    set -l title (set -q argv[1]; and echo $argv[1]; or prompt_pwd)
-    # Broken in Zellij v0.40.0: https://github.com/zellij-org/zellij/issues/3269
-    # set -q ZELLIJ; and zellij action rename-tab "$title"
-    echo "$title"
+    echo (set -q argv[1]; and echo $argv[1]; or prompt_pwd)
+
+    set -q ZELLIJ; and zellij action rename-tab \
+        (set -q argv[1]; and echo $argv[1]; or basename $SHELL)
 end
