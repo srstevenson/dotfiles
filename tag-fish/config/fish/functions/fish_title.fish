aaf1f676ecc2daa9c15f747f031b25a8b0e8d943
Scott Stevenson <scott@stevenson.io>
2024-05-04 14:29:24 +0100

    fish: Show hostname in terminal title

--- tag-fish/config/fish/functions/fish_title.fish
+++ tag-fish/config/fish/functions/fish_title.fish
@@ -1,9 +1,9 @@
 function fish_title
     if set -q argv[1]
-        echo $argv[1]
+        echo $hostname: $argv[1]
         set -q ZELLIJ; and zellij ac rename-tab (string split -f 1 " " $argv[1])
     else
-        prompt_pwd
+        echo $hostname: (prompt_pwd)
         set -q ZELLIJ; and zellij ac rename-tab fish
     end
 end
