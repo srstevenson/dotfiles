1efe0581f2e9fd27a58680fca763c4f194f232c3
Scott Stevenson <scott@stevenson.io>
2024-05-09 19:55:35 +0100

    fish: Remove hostname from terminal title

--- tag-fish/config/fish/functions/fish_title.fish
+++ tag-fish/config/fish/functions/fish_title.fish
@@ -1,9 +1,9 @@
 function fish_title
     if set -q argv[1]
-        echo $hostname: $argv[1]
+        echo $argv[1]
         set -q ZELLIJ; and zellij ac rename-tab (string split -f 1 " " $argv[1])
     else
-        echo $hostname: (prompt_pwd)
+        prompt_pwd
         set -q ZELLIJ; and zellij ac rename-tab fish
     end
 end
