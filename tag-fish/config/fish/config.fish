133ef232ecace279ff260c88419cd9f8a1d6b621
Scott Stevenson <scott@stevenson.io>
2024-05-10 13:49:53 +0100

    fish: Sync bin directory

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -7,7 +7,7 @@ set -gx HOMEBREW_BUNDLE_FILE_GLOBAL ~/.config/Brewfile
 set -gx HOMEBREW_NO_ANALYTICS 1
 test -d /opt/homebrew; and /opt/homebrew/bin/brew shellenv fish | source
 
-fish_add_path -P ~/bin ~/.local/bin ~/.cargo/bin
+fish_add_path -P ~/Dropbox/bin ~/.local/bin ~/.cargo/bin
 
 set -gx VISUAL hx
 set -gx LESSHISTFILE /dev/null
