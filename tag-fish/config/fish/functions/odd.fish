ea2a9b94356a912501207fc243910fe3a694044b
Scott Stevenson <scott@stevenson.io>
2024-06-29 19:19:05 +0100

    fish: Find files in Dropbox, not just directories

--- tag-fish/config/fish/functions/odd.fish
+++ tag-fish/config/fish/functions/odd.fish
@@ -1,4 +1,4 @@
 function odd
     set -q argv[1]; and set query $argv[1]; or set query ""
-    open (find ~/Dropbox -type d -mindepth 1 | fzy -q $query)
+    open (find ~/Dropbox -mindepth 1 | fzy -q $query)
 end
