3524b4e2b4d9c67280ac72ff87527b1ca6cebac7
Scott Stevenson <scott@stevenson.io>
2025-05-05 22:51:27 +0100

    fish,zsh: Migrate from fish back to zsh

--- tag-fish/config/fish/functions/cds.fish
+++
@@ -1,8 +0,0 @@
-function cds
-    if set -q argv[1]
-        set query $argv[1]
-    else
-        set query ""
-    end
-    cd ~/src/(git-repos | fzy -q $query)
-end
--- tag-fish/config/fish/functions/ofd.fish
+++
@@ -1,8 +0,0 @@
-function ofd
-    if set -q argv[1]
-        set query $argv[1]
-    else
-        set query ""
-    end
-    open (find ~/Dropbox -mindepth 1 | fzy -q $query)
-end
